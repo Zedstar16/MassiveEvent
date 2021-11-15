a1769ed4600990c3aef94389ffe5014c02cad457
0x00032 <53447243+0x00032@users.noreply.github.com>
2021-11-15 12:46:18 -0800

    Implemented SexySoupv2

--- src/Zedstar16/MassiveEvent/listener/EventListener.php
+++ src/Zedstar16/MassiveEvent/listener/EventListener.php
@@ -3,10 +3,24 @@
 namespace Zedstar16\MassiveEvent\listener;
 
 use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerItemUseEvent;
 
 class EventListener implements Listener
 {
+    public function onInteract(PlayerItemUseEvent $event)
+    {
 
+        $player = $event->getPlayer();
+        $item = $event->getItem();
 
-
+        if ($item->getId() == 282) {
+            if($player->getHealth() == 20){
+                
+            } else {
+                $item->pop();
+                $player->getInventory()->setItemInHand($item);
+                $player->setHealth($player->getHealth() + 8);
+            }
+        }
+    }
 }
